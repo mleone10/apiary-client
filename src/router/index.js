63407e089ec59167ce89a40c418e5c4adfac89c4
Mario Leone <mleone10@gmail.com>
2018-05-15 22:45:52 +0000

    Reinitializing project.

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-const routerOptions = [
-  { path: '/', component: 'Home' },
-  { path: '/about', component: 'About' },
-  { path: '*', component: 'NotFound' }
-]
-
-const routes = routerOptions.map(route => {
-  return {
-    ...route,
-    component: () => import(`@/components/${route.component}.vue`)
-  }
-})
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes,
-  mode: 'history'
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
